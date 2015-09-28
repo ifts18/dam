e8b3b769428b9a04e0f7c0ee3171b134536cb5f3
Leandro E. Colombo Viña (XPS) <colomboleandro@bitson.com.ar>
2015-09-28 00:46:41 -0300

    Usando escenas Bienvenida, Juego y Fin

--- disparar_monos.py
+++ disparar_monos.py
@@ -6,88 +6,108 @@ import random
 
 pilas = pilasengine.iniciar()
 
-# Variables y Constantes
-monos = []
-tiempo = 6
-fin_de_juego = False
 
-# Funciones
+class PantallaBienvenida(pilasengine.escenas.Escena):
+    def iniciar(self, mensaje):
+        self.fondo = self.pilas.fondos.Selva()
+        self.texto = pilas.actores.Texto(mensaje)
+        self.texto.y = 150
+        boton = pilas.interfaz.Boton("Empezar el juego")
+        boton.conectar(self._arrancar_juego)
 
-def crear_mono():
-    enemigo = pilas.actores.Mono()
-    enemigo.escala = 0
-    pilas.utils.interpolar(enemigo, 'escala', 0.5, duracion=0.5)
-    enemigo.aprender(pilas.habilidades.PuedeExplotar)
-
-    x = random.randrange(-320, 320)
-    y = random.randrange(-240, 240)
-
-    if x >= 0 and x <= 100:
-        x = 180
-    elif x <= 0 and x >= -100:
-        x = -180
-
-    if y >= 0 and y <= 100:
-        y = 180
-    elif y <= 0 and y >= -100:
-        y = -180
-
-    enemigo.x = x
-    enemigo.y = y
-
-    tipo_interpolacion = ['lineal',
-                          'aceleracion_gradual',
-                          'desaceleracion_gradual',
-                          'rebote_inicial',
-                          'rebote_final'
-                        ]
-
-    pilas.utils.interpolar(enemigo, 'x', 0, tiempo)
-    pilas.utils.interpolar(enemigo, 'y', 0, tiempo)
-
-    monos.append(enemigo)
-
-    if fin_de_juego:
-        return False
-    else:
-        return True
+    def _arrancar_juego(self):
+        self.pilas.escenas.EscenaJuego()
 
 
-def mono_destruido(disparo, enemigo):
-    enemigo.eliminar()
-    disparo.eliminar()
+class EscenaJuego(pilasengine.escenas.Escena):
+    def iniciar(self):
+        self.pilas.fondos.Pasto()
+        self.monos = []
+        self.tiempo = 6
+        self.puntos = pilas.actores.Puntaje(x=230, y=200,
+                                            color=pilas.colores.blanco)
+        self.puntos.magnitud = 40
+        self.pilas.actores.Sonido()
 
-    puntos.escala = 0
-    pilas.utils.interpolar(puntos, 'escala', 1, duracion=0.5)
-    puntos.aumentar(1)
+        self.torreta = pilas.actores.Torreta(enemigos=self.monos,
+                                    cuando_elimina_enemigo=self._mono_destruido)
 
-def perder(torreta, enemigo):
-    global fin_de_juego
+        # Agregamos un mono enemigo cada 1 segundo
+        self.pilas.tareas.agregar(1, self._crear_mono)
+        self.pilas.colisiones.agregar(self.torreta, self.monos, self._perder)
+        self.fin_de_juego = False
 
-    enemigo.sonreir()
-    torreta.eliminar()
+    def _crear_mono(self):
+        enemigo = pilas.actores.Mono()
+        enemigo.escala = 0
+        pilas.utils.interpolar(enemigo, 'escala', 0.5, duracion=0.5)
+        enemigo.aprender(pilas.habilidades.PuedeExplotar)
 
-    fin_de_juego = True
-    pilas.avisar("PERDISTE! Conseguiste {} puntos".format(puntos.obtener()))
+        x = random.randrange(-320, 320)
+        y = random.randrange(-240, 240)
 
-# Usar fondo estándar
-pilas.fondos.Pasto()
+        if x >= 0 and x <= 100:
+            x = 180
+        elif x <= 0 and x >= -100:
+            x = -180
 
-# Añadir un marcador
-puntos = pilas.actores.Puntaje(x=230, y=200, color=pilas.colores.blanco)
-puntos.magnitud = 40
+        if y >= 0 and y <= 100:
+            y = 180
+        elif y <= 0 and y >= -100:
+            y = -180
 
-# Añadimos el conmutador de sonido
-pilas.actores.Sonido()
+        enemigo.x = x
+        enemigo.y = y
 
-# Agregamos la torreta del jugador
-torreta = pilas.actores.Torreta(enemigos=monos,
-                                cuando_elimina_enemigo=mono_destruido)
+        pilas.utils.interpolar(enemigo, 'x', 0, self.tiempo)
+        pilas.utils.interpolar(enemigo, 'y', 0, self.tiempo)
 
-# Agregamos un mono enemigo cada 1 segundo
-pilas.tareas.agregar(1, crear_mono)
+        self.monos.append(enemigo)
 
-pilas.colisiones.agregar(torreta, monos, perder)
+        if self.fin_de_juego:
+            return False
+        else:
+            return True
+
+    def _mono_destruido(self, disparo, enemigo):
+        enemigo.eliminar()
+        disparo.eliminar()
+
+        self.puntos.escala = 0
+        pilas.utils.interpolar(self.puntos, 'escala', 1, duracion=0.5)
+        self.puntos.aumentar(1)
+
+    def _perder(self):
+        self.fin_de_juego = True
+        self.pilas.tareas.eliminar_todas()
+        for mono in self.monos:
+            mono.sonreir()
+        self.torreta.eliminar()
+        self.pilas.escenas.EscenaFin(self.puntos.obtener())
+
+
+class EscenaFin(pilasengine.escenas.Escena):
+    def iniciar(self, puntaje):
+        self.pilas.fondos.Selva()
+        mensaje = "PERDISTE! Hiciste {} puntos.".format(puntaje)
+        self.texto = pilas.actores.Texto(mensaje)
+        boton = pilas.interfaz.Boton("Volver a intentarlo")
+        boton.y = 100
+        boton.conectar(self._arrancar_juego)
+        salir = pilas.interfaz.Boton("Salir")
+        salir.y = -100
+        salir.conectar(self._salir)
+
+    def _arrancar_juego(self):
+        self.pilas.escenas.EscenaJuego()
+
+    def _salir(self):
+        pilas.terminar()
+
+pilas.escenas.vincular(PantallaBienvenida)
+pilas.escenas.vincular(EscenaJuego)
+pilas.escenas.vincular(EscenaFin)
+pilas.escenas.PantallaBienvenida("Bienvenido...")
 
 # Arrancamos el juego
 pilas.ejecutar()
